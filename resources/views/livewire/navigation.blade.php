649c71db68082172234adce7ef35eb5d6afaec79
Alejandro Echavarria <mechavarria@inespre.gob.do>
2022-09-01 13:59:01 -0400

    2022-09-1

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -39,9 +39,9 @@
                                 role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
                                 tabindex="-1">
                                 
-                                {{-- <a href="{{ route('profile.show') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1"
+                                <a href="{{ route('profile.show') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1"
                                     id="user-menu-item-0">Perfil
-                                </a> --}}
+                                </a>
                                 
                                 {{-- @can('admin.home')
                                     <a href="{{ route('admin.home') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1"
