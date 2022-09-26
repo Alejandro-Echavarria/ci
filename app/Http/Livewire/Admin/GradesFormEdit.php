32b8e098cbbcda8620e34651012578290709e295
Alejandro Echavarria <mechavarria@inespre.gob.do>
2022-09-26 16:01:17 -0400

    mechavaria-2022-09-26-work

--- app/Http/Livewire/Admin/GradesFormEdit.php
+++ app/Http/Livewire/Admin/GradesFormEdit.php
@@ -3,12 +3,15 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\Grade;
+use App\Models\Quater;
 use App\Models\Subject;
 use Livewire\Component;
+use Illuminate\Support\Str;
 
 class GradesFormEdit extends Component
 {
     public $subjects;
+    public $subjectsId;
     public $grades;
     public $quater;
     public $name;
@@ -16,37 +19,69 @@ class GradesFormEdit extends Component
     // Validations rules
     protected $rules = [
         'name'       => 'required|min:1|max:60',
-        'subjects.*' => 'required|integer'
+        // 'subjects.*' => 'required|integer'
     ];
 
     protected $validationAttributes  = [
-        'subjects.*' => ''
+        // 'subjects.*' => ''
     ];
 
     public function mount()
-    {
+    {   
+        $this->name = $this->quater['name'];
         $this->grades = Grade::all();
         $this->subjects = Subject::where('quater_id', $this->quater['id'])->get()->toArray();
+        $this->subjectsId = Subject::where('quater_id', $this->quater['id'])->pluck('id');
     }
 
     public function addSubject()
     {
         if (count($this->subjects) < 15) {
             
-            $this->subjects[] = [];
+            $this->subjects[] = Subject::create(['quater_id' => $this->quater['id'], 'grade_id' => 1]);
+            $this->mount();
         }else{
-            session()->flash('info', 'El número máximo de materias son 15 por período');
+            session()->flash('error', 'El número máximo de materias son 15 por período');
         }
     }
 
-    public function removeSubject($key)
+    public function removeSubject($key, $id)
     {
         unset($this->subjects[$key]);
         array_values($this->subjects);
+        
+        $subject = Subject::find($id);
+        $subject->delete();
     }
 
     public function render()
     {
         return view('livewire.admin.grades-form-edit');
+    }
+
+    public function save()
+    {
+        $totalQuaters = Quater::all();
+           
+        if (count($totalQuaters) < 15) {
+            
+            $this->validate();
+            $slug = Str::slug($this->name);
+
+            $quater = $this->quater;
+
+            $quater->update(['name' => $this->name, 
+                                      'slug' => $slug]);
+    
+            foreach ($this->subjects as $subject) {
+                
+                Subject::where('id', 99)->update(['grade_id' => request('subjects[]')]);
+            }
+    
+            return redirect()->route('admin.quaters.edit', $quater)->with('info', 'El cuatrimestre ('. $quater->name .') se creó con éxito.');
+        }else{
+            session()->flash('error', 'El número máximo de cuatrimestres permitidos son 15');
+        }
+
     }
 }
--- resources/views/livewire/admin/grades-form-edit.blade.php
+++ resources/views/livewire/admin/grades-form-edit.blade.php
@@ -3,6 +3,9 @@
         @if (session('info'))
             <x-admin.alerts.alerts :message="session('info')" :type="'red-color'" />
         @endif
+        @if (session('error'))
+            <x-admin.alerts.alerts :message="session('error')" :type="'red-color'" />
+        @endif
         <div>
             <label 
                 for="name" 
@@ -23,34 +26,58 @@
                 <small class="text-red-500">{{$message}}</small>
             @enderror
         </div>
-        @foreach ($subjects as $key =>  $subject)
-            <div>
-                <div class="flex gap-3">
-                    <div class="flex">
-                        <p class="block text-sm font-medium text-gray-900 dark:text-gray-300 place-self-center">Materia&nbsp;-&nbsp;{{ $key+1 }}</p>
-                    </div>
-                    {{-- <select id="subjects[{{$key}}]" name="subjects[{{$key}}]" wire:model="subjects.{{$key}}" class="@error('grades.'.$key.'.select') border border-red-400 @enderror w-full p-2.5 text-sm text-gray-700 border-1 dark:border-gray-600 text-white-500 rounded-xl shadow-sm focus:outline-none dark:text-white-400 dark:placeholder:text-white-600 dark:bg-gray-700 dark:text-white transition ease-in-out">
-                        <option value="">-- Selecciona una opci&oacute;n --</option>
-                        @foreach ($grades as $grade)
-                            <option value="{{ $grade->id }}" {{old("subjects[$key]" , $grade->name)==$grade->id ? 'selected' : ''}}>{{ $grade->name }}</option>
-                        @endforeach
-                    </select> --}}
-                    {!! Form::select("subjects[{{$key}}]", $grades, null) !!}
+        @if (count($subjects))
+        
+            @foreach ($subjects as $key =>  $subject)
+                @php
+                    $id = $subjectsId[$key];
+                @endphp
+                <div>
+                    <div class="flex gap-3">
+                        <div class="flex">
+                            <p class="block text-sm font-medium text-gray-900 dark:text-gray-300 place-self-center">Materia&nbsp;-&nbsp;{{ $key+1 }}</p>
+                        </div>
+                        {{-- wire:model="subjects.{{ $key }}" --}}
+                        {{-- <select wire:ignore="subjects.{{$id}}" id="subjects[{{ $id }}]" 
+                        name="subjects[{{ $id }}]" class="w-full p-2.5 text-sm text-gray-700 border-1 dark:border-gray-600 text-white-500 rounded-xl shadow-sm focus:outline-none dark:text-white-400 dark:placeholder:text-white-600 dark:bg-gray-700 dark:text-white transition ease-in-out">
+                            <option value="">-- Selecciona una opci&oacute;n --</option>
+                            @foreach ($grades as $grade)
+                                <option value="{{ $grade->id }}">{{ $grade->name }}</option>
+                            @endforeach
+                        </select> --}}
+                        <select id="subjects[{{ $id }}]" 
+                        name="subjects[{{ $id }}]" class="w-full p-2.5 text-sm text-gray-700 border-1 dark:border-gray-600 text-white-500 rounded-xl shadow-sm focus:outline-none dark:text-white-400 dark:placeholder:text-white-600 dark:bg-gray-700 dark:text-white transition ease-in-out">
+                            <option value="">-- Selecciona una opci&oacute;n --</option>
+                            @foreach ($grades as $grade)
+                                <option value="{{ $grade->id }}" {{ $grade->id == $subject['grade_id'] ? "selected" : '' }}>{{ $grade->name }}</option>
+                            @endforeach
+                        </select>
 
-                    @if ($key === 0)
-                        <button type="button" class="text-white color-secundario focus:outline-none font-medium rounded-xl text-sm w-auto sm:w-auto px-5 py-2.5 text-center cursor-pointer dark:hover:bg-blue-700" wire:click.prevent="addSubject">+</button>
-                    @else
-                        <button type="button" class="text-white focus:outline-none font-medium rounded-xl text-sm w-auto sm:w-auto px-5 py-2.5 text-center red-color cursor-pointer dark:hover:bg-red-300" wire:click.prevent="removeSubject({{ $key }})">-</button>
-                    @endif           
+                        @if ($key === 0)
+                            <button type="button" class="text-white color-secundario focus:outline-none font-medium rounded-xl text-sm w-auto sm:w-auto px-5 py-2.5 text-center cursor-pointer dark:hover:bg-blue-700" wire:click.prevent="addSubject">+</button>
+                        @else
+                            <button type="button" class="text-white focus:outline-none font-medium rounded-xl text-sm w-auto sm:w-auto px-5 py-2.5 text-center red-color cursor-pointer dark:hover:bg-red-300" wire:click.prevent="removeSubject({{ $key }}, {{ $id }})">-</button>
+                        @endif           
+                    </div>
+                    @error('subjects.'. $key)
+                        <small class="text-red-500">{{$message}}</small>
+                    @enderror
                 </div>
-                @error('subjects.'. $key)
-                    <small class="text-red-500">{{$message}}</small>
-                @enderror
-            </div>
-        @endforeach
+            @endforeach
+        @else
+            
+        @endif
+    </div>
+    @if (session('error'))
+        <small class="text-red-500">{{ session('error') }}</small>
+    @endif
+    <div class="w-full flex justify-end">
+        <a href="{{ route('admin.quaters.index') }}" class="font-medium text-sm text-white hover:underline px-5 py-2.5 text-center">Regresar</a>
+        <button wire:click="save()" type="button" class="text-white color-secundario focus:outline-none font-medium rounded-xl text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:hover:bg-blue-700 cursor-pointer">
+            Actualizar
+        </button>
     </div>
 </div>
-
 
 {{-- <div>
     <div class="grid grid-cols-1 sm:grid-cols-1 gap-3 mb-3">
