d5ce035f66b7f463d8754a109dbe10c6e428d070
Alejandro Echavarria <mechavarria@inespre.gob.do>
2022-11-22 10:24:48 -0400

    theme-color - test:1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,18 +4,19 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="theme-color" media="(prefers-color-scheme: light)" content="#ffffff">
-        <meta name="theme-color" media="(prefers-color-scheme: dark)"  content="#000000">
+        <script>
+            try {
+              if (localStorage.dark == 1 || (!('dark' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.documentElement.classList.add('dark')
+                document.querySelector('meta[name="theme-color"]').setAttribute('content', '#0B1120')
+              } else {
+                document.documentElement.classList.remove('dark')
+              }
+            } catch (_) {}
+          </script>
         <meta name="author" content="Manuel Echavarria">
         <title>{{ config('app.name', 'MaetDev') }}</title>
         <meta name="description" content="Calcula y gestiona de manera fácil tu índice académico."/>
-        <script>
-            if (localStorage.dark == 1) {
-                document.documentElement.classList.add('dark');
-            } else {
-                document.documentElement.classList.remove('dark');
-            }
-        </script>
         
         <link rel="shortcut icon" href="{{ asset('img/11.ico') }}">
         <!-- Fonts -->
